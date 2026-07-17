111b0abddff9002c38f20b6f999207f5ac5c8c4d
Soikoth-Biswas <soikothbiswas12@gmail.com>
2026-07-17 12:21:48 -0700

    first singler

---
+++ server/soikoth/biswas.js
@@ -0,0 +1,7 @@
+// Function that takes a name and prints a greeting
+function greetUser(name) {
+    console.log("Hello, " + name + "! Welcome to JavaScript.");
+}
+
+// Call the function
+greetUser("Alex");
